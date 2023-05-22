e95c2eb3f16ead7ea7f1e19942222af192e4c784
Paul Gigel <p.gigel@bbw-fi.de>
2023-05-22 16:13:47 +0200

    leel

--- OS/Switch_to_32bit.asm
+++ OS/Switch_to_32bit.asm
@@ -1,5 +1,6 @@
 msg db 'Hello Worldddddddddddddd!',10,13,'luuul',10,13,0
 switch_to_32bit:
+	pusha
 										; Update the segment registers
 	mov ax, cs							; Accumulator = CodeSegment
 	mov ds, ax							; DataSegment = Accumulator
@@ -9,8 +10,11 @@ switch_to_32bit:
 print:
 	lodsb								; Load String Byte
 	cmp al, 0							; If AL=0 then hang
+	je return
 	mov ah, 0Eh							; Print AL
 	mov bx, 7
 	int 10h								; Interupt (call Video Service)
 	jmp print							; Print next character
+return:	
+	popa
 	ret
