3af50f9fc263ebe3156ac98003e3a3241bd4a300
Paul Gigel <p.gigel@bbw-fi.de>
2023-05-26 20:28:51 +0200

    now

--- OS/Kernel.c
+++ OS/Kernel.c
@@ -1,4 +1,12 @@
+struct Video_Mem    {
+    char* code;
+    char* color;
+};
 void main() {
-	char* video_memory = (char*)0xb8000;
-	*video_memory = 'X';
+    struct Video_Mem videoMem = {(char*)0xb8000, (char*)0xb8001};
+    *(Video_Mem.code) = 'Z';
+	char* video_memory1 = (char*)0xb8002;
+	*video_memory1 = 'A';
+    char* video_memory2 = (char*)0xb8004;
+    *video_memory2 = 'B';
 }
